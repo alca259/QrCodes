065f180b75fe6062c6d149161e0c04286e00bab5
Alca259 <alca259>
2018-12-18 12:43:16 +0100

    Added scanner for barcodes

--- MediaFrameQrProcessing/ZXing/ZxingBarcodeDecoder.cs
+++ MediaFrameQrProcessing/ZXing/ZxingBarcodeDecoder.cs
@@ -5,29 +5,29 @@
     using Windows.Storage.Streams;
     using ZXing;
 
-    public static class ZXingQRCodeDecoder
+    public static class ZXingBarcodeDecoder
     {
         static BarcodeReader barcodeReader;
 
-        static ZXingQRCodeDecoder()
+        static ZXingBarcodeDecoder()
         {
             barcodeReader = new BarcodeReader();
             barcodeReader.Options.PureBarcode = false;
             barcodeReader.Options.Hints.Add(DecodeHintType.TRY_HARDER, true);
-            barcodeReader.Options.PossibleFormats = new BarcodeFormat[] { BarcodeFormat.QR_CODE };
+            barcodeReader.Options.PossibleFormats = new BarcodeFormat[] { BarcodeFormat.All_1D };
 
             barcodeReader.Options.TryHarder = true;
         }
 
-        public static Result DecodeBufferToQRCode(byte[] buffer, int width, int height, BitmapFormat bitmapFormat)
+        public static Result DecodeBufferToCode(byte[] buffer, int width, int height, BitmapFormat bitmapFormat)
         {
             var zxingResult = barcodeReader.Decode(buffer, width, height, bitmapFormat);
             return (zxingResult);
         }
 
-        public static Result DecodeBufferToQRCode(IBuffer buffer, int width, int height, BitmapFormat bitmapFormat)
+        public static Result DecodeBufferToCode(IBuffer buffer, int width, int height, BitmapFormat bitmapFormat)
         {
-            return (DecodeBufferToQRCode(buffer.ToArray(), width, height, bitmapFormat));
+            return (DecodeBufferToCode(buffer.ToArray(), width, height, bitmapFormat));
         }
     }
 }
